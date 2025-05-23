404c68b968cd57224be3f9358c79e7895dd530a3
Moritz Scholjegerdes <moritz-scholjegerdes@gmx.com>
2025-05-23 02:14:42 +0200

    read more ADCs for DEBUG infos

--- SW_BugWiper/SW_BugWiper.ino
+++ SW_BugWiper/SW_BugWiper.ino
@@ -6,15 +6,6 @@
 
 #define FIRMWARE_VERSION "V0.0.2"
 
-// Pin discription
-// ESP32-Wroom-32: 
-// follwing pins are not allowed to use: 0 (Bootselect); 2 Board LED / must be low on boot; (1 & 3 UART USB-Serial); 
-// (5 must be high during boot);(15 Debugging Log on U0TXD During Booting);(6, 7, 8, 9, 10 & 11 connected to Flash);
-// Input Only Pins : 34,35,36,39
-// ESP32-S3-Wroom-1:
-// follwing pins are difficult to use: 0 (Bootselect); 3 (Strapping Pins Floating) ; 19 & 20 (USB-Jtag); 35,36&37 (Octal PSRAM (8MB));
-// 39,40,41&42 (JTAG); 43 & 44 (UART 0 for serial debug interface); 45 & 46 (Strapping Pins / Pull-down)  48 Board LED
-
 //SD Card
   #define SD_Detect_PIN 2
 
@@ -39,10 +30,18 @@ void loop() {
     //read_Buttons();
     //BugWiper_calculate(0, 0, 0);
     delay(1000);
-    DEBUG_INFO("ADC value = " + String(BW_ADC_current_sense) + " HB1:" + String(BW_ADC_btn_hb1)+" HB2:" + String(BW_ADC_btn_hb2));
+    DEBUG_INFO("ADC Current = " + String(BW_ADC_current_mA) + " HB1:" + String(BW_ADC_btn_hb1)+" HB2:" + String(BW_ADC_btn_hb2));
     DEBUG_INFO("Encoder count = " + String((int32_t)motor_enc_count));
     DEBUG_INFO("Position = " + String(BW_position) + " Speed:" + String(BW_speed));
     DEBUG_INFO("State:" + String(BW_state_machine_state));
+    float temp;
+    uint16_t adc_temp;
+    adc_temp = analogReadMilliVolts(ADC_NTC_PIN);
+    temp = (float)adc_temp * 4095.0 / 3100.0;
+    adc_temp = (uint16_t) temp;
+    temp = ((float)adc_temp*(float)adc_temp*(float)adc_temp*(-2.87638e-9))+((float)adc_temp*(float)adc_temp*(2.01243e-5))+((-0.0702)*(float)adc_temp)+109.013;
+    DEBUG_INFO("ADC VBat= " + String((float)((float)analogReadMilliVolts(ADC_VBat_PIN)*0.0078)) + " NTC:" + String((float)analogReadMilliVolts(ADC_NTC_PIN)) + " =" + String(temp));
+    DEBUG_INFO(" ");
 }
 
 
