624520f417ab8e39e3f055b066f37d66b907e09f
Moritz Scholjegerdes <moritz-scholjegerdes@gmx.com>
2025-05-24 02:03:47 +0200

    change debug infos to use witch serial plotter

--- SW_BugWiper/SW_BugWiper.ino
+++ SW_BugWiper/SW_BugWiper.ino
@@ -30,12 +30,11 @@ void loop() {
     //read_Buttons();
     //BugWiper_calculate(0, 0, 0);
     delay(1000);
-    DEBUG_INFO("ADC Current = " + String(BW_ADC_current_mA) + " HB1:" + String(BW_ADC_btn_hb1)+" HB2:" + String(BW_ADC_btn_hb2));
-    DEBUG_INFO("Encoder count = " + String((int32_t)motor_enc_count));
-    DEBUG_INFO("Position = " + String(BW_position) + " Speed:" + String(BW_speed));
+    DEBUG_INFO("ADC_Current:" + String(BW_ADC_current_mA) + " HB1:" + String(BW_ADC_btn_hb1) + " HB2:" + String(BW_ADC_btn_hb2));
+    DEBUG_INFO("Encoder_count:" + String((int32_t)motor_enc_count));
+    DEBUG_INFO("Position:" + String(BW_position) + " Speed:" + String(BW_speed));
     DEBUG_INFO("State:" + String(BW_state_machine_state));
-    DEBUG_INFO("ADC VBat= " + String(BW_ADC_V_Bat) + " NTC:" + String(BW_ADC_T_ntc_degree));
-    DEBUG_INFO("SW_C_loose= "+ String(cable_loose));
-    DEBUG_INFO(" ");
+    DEBUG_INFO("ADC_VBat:" + String(BW_ADC_V_Bat) + " NTC:" + String(BW_ADC_T_ntc_degree));
+    DEBUG_INFO("SW_loose:"+ String(cable_loose));
 }
 
